961ded12e2c2ea412ef0098dcf6f1d2bded8b2d1
Ian Applebaum <Ian.tyler@electricbluefish.com>
2021-02-11 23:13:16 -0500

    iOS13 seems to be required for SwiftUI

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUI Apple Watch Decimal Pad",
-	platforms: [.watchOS(.v7)],
+	platforms: [.watchOS(.v7),.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
