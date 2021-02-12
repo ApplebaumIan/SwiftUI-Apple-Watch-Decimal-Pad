5064bb9abd57087d9b577ce869a37692dbd13b2e
Ian Applebaum <Ian.tyler@electricbluefish.com>
2021-02-11 23:08:38 -0500

    package is now watchOS 7 only.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUI Apple Watch Decimal Pad",
-	platforms: [.watchOS(.v6),.iOS(.v13)],
+	platforms: [.watchOS(.v7)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
