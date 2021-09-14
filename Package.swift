4b9a4493e537965f0b7d835b1418f6399d6e2ceb
Ian Applebaum <Ian.tyler@electricbluefish.com>
2021-09-14 09:17:46 -0400

    This is a watchOS only package.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUI Apple Watch Decimal Pad",
-    platforms: [.iOS(.v14),.watchOS(.v7)],
+    platforms: [.watchOS(.v7)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
